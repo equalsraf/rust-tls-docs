78a9b272ab5d24a7fedbb34c35c3e19977af66b4
rust-tls CI <rust-tls@travis>
2016-01-27 00:21:52 +0000

    Build docs/tb-what from 19a03176afa18a32fc66cc1953e90acc069696b3

---
+++ tb-what/implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tls'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='tls/struct.TlsStream.html' title='tls::TlsStream'>TlsStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
