78e8fd156bfbe92a834854bbc28d8d597984ef6d
rust-tls CI <rust-tls@travis>
2016-01-27 13:30:19 +0000

    Build docs/master from 8d5692428602eb7738dbd93a4c09123ae13e30a9

--- master/implementors/core/ops/trait.Drop.js
+++ master/implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['tls'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tls/struct.TlsStream.html' title='tls::TlsStream'>TlsStream</a>",];
+implementors['libc'] = [];implementors['tls'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tls/raw/struct.TlsConfig.html' title='tls::raw::TlsConfig'>TlsConfig</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tls/raw/struct.TlsContext.html' title='tls::raw::TlsContext'>TlsContext</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tls/struct.TlsStream.html' title='tls::TlsStream'>TlsStream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
