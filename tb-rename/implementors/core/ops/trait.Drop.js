a92d9478e41369207cf3aa2c18484fbbf6f0b908
telos CI <telos@travis>
2016-02-11 00:03:39 +0000

    Build docs/tb-rename from 739ea7b1569644e591bcb3b42de16370713014ed

---
+++ tb-rename/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['telos'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='telos/raw/struct.TlsConfig.html' title='telos::raw::TlsConfig'>TlsConfig</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='telos/raw/struct.TlsContext.html' title='telos::raw::TlsContext'>TlsContext</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='telos/struct.TlsStream.html' title='telos::TlsStream'>TlsStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
