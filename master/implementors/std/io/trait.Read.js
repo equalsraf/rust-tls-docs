7192d43147d433b83806cb8e7b501d03fc6abbf9
telos CI <telos@travis>
2016-02-11 00:51:02 +0000

    Build docs/master from ef34b8d3ce9eca935ca7b66a68c55bc9ed4a7ca9

--- master/implementors/std/io/trait.Read.js
+++ master/implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tls'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='tls/struct.TlsStream.html' title='tls::TlsStream'>TlsStream</a>",];
+implementors['telos'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='telos/struct.TlsStream.html' title='telos::TlsStream'>TlsStream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
